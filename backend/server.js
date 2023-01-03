9583c27b8f75c730aa87aa286b47ea2048c9b452
Eric Sauber <ericjsauber@gmail.com>
2023-01-03 12:18:20 -0600

    server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+	res.status(200).json({ message: 'yes' });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
