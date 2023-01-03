fae08f606d533bdebcada9aa2b6583a5d3ddff7e
Eric Sauber <ericjsauber@gmail.com>
2023-01-03 12:19:33 -0600

    test

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 app.get('/api/goals', (req, res) => {
-	res.status(200).json({ message: 'yes' });
+	res.status(200).json({ message: 'yes2' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
