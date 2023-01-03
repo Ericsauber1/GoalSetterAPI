f270da7cb14c0afee798b93f7fcc31879a187946
Eric Sauber <ericjsauber@gmail.com>
2023-01-03 14:01:51 -0600

    routes added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 4000;
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
 
-app.get('/api/goals', (req, res) => {
-	res.status(200).json({ message: 'yes2' });
-});
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+ 
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler);
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
